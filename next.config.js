c68e9687262d945ebfb69ca1e72f6d7ca23c8f71
Innerhard <innerhard@gmail.com>
2021-11-07 04:33:54 +0300

    delete

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
     reactStrictMode: true,
-    basePath: '/avkservice.github.io/',
-    assetPrefix: '/avkservice.github.io/',
+    basePath: '/avkservice.github.io',
+    assetPrefix: '/avkservice.github.io',
 }
